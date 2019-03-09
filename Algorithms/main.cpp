5710b7fd4440f1bbfd7155539537c281a607c0b8
bence <bence7999@gmail.com>
2019-03-09 12:02:48 +0100

    Extend task 1

--- Algorithms/main.cpp
+++ Algorithms/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
+#include "StacksAndQueues.h"
+
 
 int main(int argc, char* argv[]) {
+
+	int stack[MAX01];
+
+	std::cout << isEmpty() << std::endl;
+	push(stack, 4);
+	push(stack, 1);
+	push(stack, 3);
+	pop(stack);
+	push(stack, 8);
+	pop(stack);
+	std::cout << isEmpty() << std::endl;
+	push(stack, 6);
+	push(stack, 9);
+	push(stack, 15);
+	std::cout << isEmpty() << std::endl;
+	push(stack, 1);
+	push(stack, 10);
 
 	return 0;
 }
